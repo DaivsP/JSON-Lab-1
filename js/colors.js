a799619a7af1f8d412e872428cee5883c02218d5
Davis <davisperchik@gmail.com>
2019-03-25 16:32:37 -0400

    actually working now

--- js/colors.js
+++ js/colors.js
@@ -1,16 +1,16 @@
-var colorsArray : {
+var colorsArray = {
     colors : [
         {
-            "name":"green",
-            "red":"64",
-            "green":"175",
-            "blue":"63",
+            name:"green",
+            red:"64",
+            green:"175",
+            blue:"63",
         },
         {
-            "name":"yellow",
-            "red":"239",
-            "green":"183",
-            "blue":"62"
+            name:"yellow",
+            red:"239",
+            green:"183",
+            blue:"62"
         },
         {
             name:"red",
@@ -19,16 +19,16 @@ var colorsArray : {
             blue:"35"
         },
         {
-            "name":"blue",
-            "red":"19",
-            "green":"71",
-            "blue":"144",
+            name:"blue",
+            red:"19",
+            green:"71",
+            blue:"144",
         },
         {
-            "name":"orange",
-            "red":"253",
-            "green":"92",
-            "blue":"48"
+            name:"orange",
+            red:"253",
+            green:"92",
+            blue:"48"
         },
     ]
 };
